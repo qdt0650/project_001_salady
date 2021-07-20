e6dc50f7a53de3bc51223869397d350ca176afb8
qdt0650 <qdt0650@naver.com>
2021-07-20 15:19:02 +0900

    반응형코딩(메인)
    반응형코딩(메인)

---
+++ code/js/src/portfolio_sub_menu-2.js
@@ -0,0 +1,16 @@
+// js_portfolio_sub_menu-2.js
+
+(function($){
+  // jQuery
+
+  // 변수
+  var headBox = $('#headBox');
+  var naviBox = $('#naviBox');
+  var sideBox = $('#sideBox');
+
+  var topBtn = $('.top_btn');
+
+  topBtn.find('a').on('click', function(e){
+    e.preventDefault();
+    $('html,body').animate({scrollTop : 0 });
+  });
